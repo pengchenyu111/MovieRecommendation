e22af63bcc884f6b98223db5210209db3d796e79
pengchenyu <1106998960@qq.com>
2020-12-11 16:26:46 +0800

    处理插入subjects表的主键冲突问题.

--- MovieRecommendation-Spider/DouBanSpider/DouBanSpider/db_handler.py
+++ MovieRecommendation-Spider/DouBanSpider/DouBanSpider/db_handler.py
@@ -17,7 +17,7 @@ cursor = connection.cursor()
 
 # 在获取完电影简要信息后，通过这里提取并存入豆瓣id
 def update_movie_subject():
-    sql_1 = "INSERT INTO subjects(douban_id) SELECT douban_id FROM movie_brief_introduction;"
+    sql_1 = "INSERT INTO subjects(douban_id) SELECT douban_id FROM movie_brief_introduction WHERE douban_id NOT IN (SELECT douban_id FROM subjects);"
     sql_2 = "UPDATE subjects SET type = 'movie';"
     try:
         row_1 = cursor.execute(sql_1)

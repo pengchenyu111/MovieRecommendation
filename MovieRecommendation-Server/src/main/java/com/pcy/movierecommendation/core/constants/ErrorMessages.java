44d11b5764378d68a5ec3945ff2e85ac66352e4d
pengchenyu <1106998960@qq.com>
2020-12-19 00:07:44 +0800

    添加请求结果为空的错误码.

--- MovieRecommendation-Server/src/main/java/com/pcy/movierecommendation/core/constants/ErrorMessages.java
+++ MovieRecommendation-Server/src/main/java/com/pcy/movierecommendation/core/constants/ErrorMessages.java
@@ -10,6 +10,7 @@ public class ErrorMessages {
 
     public static final String REQUEST_SUCCESS = "请求成功";
     public static final String REQUEST_FAIL = "请求失败";
+    public static final String QUERY_NULL = "查询结果为空";
 
 
 }

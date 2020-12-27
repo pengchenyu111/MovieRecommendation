c65e37f2f676d508dcb724ad66e6cd3e1d7bf286
pengchenyu <1106998960@qq.com>
2020-12-27 21:56:07 +0800

    添加ElasticSearch滚动查询公函.

--- MovieRecommendation-Server/src/main/java/com/pcy/movierecommendation/es/BaseElasticSearchService.java
+++ MovieRecommendation-Server/src/main/java/com/pcy/movierecommendation/es/BaseElasticSearchService.java
@@ -1,13 +1,13 @@
 package com.pcy.movierecommendation.es;
 
 import com.alibaba.fastjson.JSON;
+import org.apache.commons.lang.ArrayUtils;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.*;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -18,6 +18,7 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.Scroll;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.slf4j.Logger;
@@ -153,6 +154,60 @@ public class BaseElasticSearchService {
             result.add(JSON.parseObject(hit.getSourceAsString(), (Type) c));
         }
         elasticSearchVo.setResultList(result);
+        return elasticSearchVo;
+    }
+
+
+    /**
+     * 滚动查询
+     * 处理返回数据较多而不想分页的情况
+     *
+     * @param indexName 索引名
+     * @param builder   查询参数
+     * @param c         结果类对象   调用者通过 Xxx.class 获取
+     * @return 对象列表
+     * @apiNote 经测试，这种方法效率很低、耗时高，不到万不得已别使用这个接口
+     */
+    public <T> ElasticSearchVo<T> scrollSearch(String indexName, SearchSourceBuilder builder, Class<?> c) throws IOException {
+        // 初始化Scroll
+        final Scroll scroll = new Scroll(TimeValue.timeValueMinutes(1L));
+        // 设置查询条件
+        SearchRequest searchRequest = new SearchRequest(indexName);
+        searchRequest.scroll(scroll);
+        searchRequest.source(builder);
+        // 发起请求获取结果
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        String scrollId = searchResponse.getScrollId();
+        logger.info("滚动查询id======>" + scrollId);
+        SearchHit[] searchHits = searchResponse.getHits().getHits();
+        SearchHit[] resultList = new SearchHit[0];
+        resultList = (SearchHit[]) ArrayUtils.addAll(searchHits, resultList);
+        // 通过循环调用Search Scroll API检索所有搜索结果，直到没有文档返回
+        while (searchHits != null && searchHits.length > 0) {
+            // 创建一个新的SearchScrollRequest保存最后返回的滚动标识符和滚动间隔
+            SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+            scrollRequest.scroll(scroll);
+            searchResponse = restHighLevelClient.scroll(scrollRequest, RequestOptions.DEFAULT);
+            scrollId = searchResponse.getScrollId();
+            searchHits = searchResponse.getHits().getHits();
+            resultList = (SearchHit[]) ArrayUtils.addAll(searchHits, resultList);
+        }
+        // 写入结果
+        ElasticSearchVo<T> elasticSearchVo = new ElasticSearchVo<>();
+        elasticSearchVo.setTotal(Long.parseLong(String.valueOf(resultList.length)));
+        elasticSearchVo.setMaxScore(resultList[0].getScore());
+        List<T> result = new ArrayList<>(resultList.length);
+        for (SearchHit hit : resultList) {
+            result.add(JSON.parseObject(hit.getSourceAsString(), (Type) c));
+        }
+        elasticSearchVo.setResultList(result);
+        // 滚动完成后，清除滚动上下文
+        ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+        clearScrollRequest.addScrollId(scrollId);
+        ClearScrollResponse clearScrollResponse = restHighLevelClient.clearScroll(clearScrollRequest, RequestOptions.DEFAULT);
+        if (!clearScrollResponse.isSucceeded()) {
+            logger.info("上下文清除失败");
+        }
         return elasticSearchVo;
     }
 
